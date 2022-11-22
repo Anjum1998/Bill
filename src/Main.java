e2ea3a18be74b3a86a921d6752d13b2bbf1663c9
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-22 14:33:13 +0530

    update

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,11 @@ public class Main {
                     int tea = input.nextInt();
                     c.tea = tea;
                     break;
+                case 2:
+                    System.out.println("Enter count of coffee");
+                    int coffee = input.nextInt();
+                    c.coffee = coffee;
+                    break;
 
             }
 //            System.out.println("do you wish to continue? true / false");
