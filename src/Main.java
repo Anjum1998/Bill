98ad2bc13b6a647f9a4ed87bbae039d089a07e49
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-22 14:27:21 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class Customer{
+    String name;
+    long phnNo;
+    int tea = 0;
+    int coffee = 0;
+    int snacks = 0;
+    int idli = 0;
+    int dosa = 0;
+    int total = 0;
+    public int generateBill(String name, long phnNo){
+        this.name = name;
+        this.phnNo = phnNo;
+        total = this.tea * 10 + this.coffee * 15 + this.snacks * 10 + this.idli * 8 + this.dosa * 6;
+        return total;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Customer> customers = new ArrayList<Customer>();
+        Customer c = new Customer();
+        boolean ch;
+        do {
+            System.out.println("select your items \n 1.Tea-10 \n 2.Coffee-15 \n 3.Snacks-10 \n 4.dli-8 \n 5.dosa\n 6.Generate Bill \n 7.view all transaction \n 8.exit");
+            Scanner input = new Scanner(System.in);
+            int n = input.nextInt();
+            switch (n) {
+
+            }
+//            System.out.println("do you wish to continue? true / false");
+//            ch = input.nextBoolean();
+        } while (true);
+    }
+}
